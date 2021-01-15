01571a350e97f54ea178620dc8406611fab72f3e
hisadomi <hb.kmaia93@icloud.com>
2021-01-15 15:34:00 +0900

    EventListener

---
+++ EventListener/eventListener.js
@@ -0,0 +1,29 @@
+const uploadImageModule = (() => {
+  const inputElement = document.getElementById("image")
+  const previewElement = document.getElementById("image-preview")
+
+  inputElement.addEventListener("change", event => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    //type="file"を指定されたinput要素のchangeイベントは「ファイルのリスト」を返す
+    const file = event.target.files[0];
+
+    //ファイルが存在しないか、ファイル形式がimageではない時
+    if (!file || !file.type.match(/image\/*/)) {
+      alert("画像ファイルではありません");
+      return false;
+    }
+
+    //FileReaderのインスタンスを生成（ローカルファイルを読み込むオブジェクト）
+    const reader = new FileReader();
+
+    reader.addEventListener("load", event => {
+      //event.targe.result  base64エンコードされた画像データ
+      previewElement.setAttribute("src", event.target.result)
+    })
+
+    //セットされたオブジェクトを読み込む
+    reader.readAsDataURL(file);
+  })
+})();
